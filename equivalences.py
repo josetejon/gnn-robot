7a1bc7ead6e16df0efd214a2338c5aba9bc68488
José Tejón Moreno <josetejon89@gmail.com>
2023-11-28 21:19:09 +0100

    Adding the robot notebook

--- equivalences.py
+++ equivalences.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 class DSREdgeType(Enum):
     RT=1
@@ -27,3 +28,87 @@ class DSREdgeType(Enum):
     following_a=24
     lost=25
     testtype_e=26
+    is_not_waiting=27
+    stopped=28
+    is_charging=29
+    is_not=30
+    has_not=31
+    is_in=32
+    is_performing=33
+    wants_talking=34
+    navigating = 35
+
+
+class ProjectId:
+    id: str
+
+class Exists:
+    show: bool
+
+class Person(ProjectId, Exists):
+    name: str
+
+class Position:
+    x: float
+    y: float
+
+class Battery(ProjectId, Exists):
+    level: float
+
+class Server(ProjectId, Exists):
+    action: str
+
+class Joystick(ProjectId, Exists):
+    connected: bool
+
+class Navigation:
+    running: bool
+    x_target: bool
+    y_target: bool
+
+class speaking(ProjectId, Exists):
+    running: bool
+
+class Answer(ProjectId, Exists):
+    waiting: bool
+
+class World(ProjectId, Exists):
+    id: str
+
+class Transform(ProjectId, Exists):
+    id: str
+
+class Room(ProjectId, Exists):
+    id: str
+    name: Optional[str]
+
+class DifferentialRobot(ProjectId, Exists):
+    id: str
+
+class OmniRobot(ProjectId, Exists):
+    id: str
+
+class Robot(ProjectId, Exists):
+    id: str
+
+class PathToTarget(ProjectId, Exists):
+    id: str
+
+class intention(ProjectId, Exists):
+    id: str
+    action: str
+
+class rgbd(ProjectId, Exists):
+    id: str
+    action: str
+
+class State(Enum):
+    pending: 0
+    active: 1
+
+class Action(Enum):
+    CHARGING_BATERY_IN_HOSPITAL: 0
+    GOING_TO_PATIENT_ROOM_TO_CONNECT_WITH_RELATIVE: 1
+
+class KnownPlaces(Enum):
+    Consulta1: 0
